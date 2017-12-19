9af598a758c0f8142523a7633188222c2f4f07c4
SiGMan <sigman78@gmail.com>
2017-12-19 17:02:29 +0200

    UI initialization hooks figured out i need it to put in one place

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,30 @@
 //
 import $ from 'jquery';
 
+// All HTML tinkery goes here
+// 
 class UI {
+
+    static init(onResize = null, onFocus = null, onUnfocus = null) {
+        // capture window resize
+        if (onResize !== null) {
+            $(window).resize(onResize);
+            $(window).on("orientationchange", onResize);
+        }
+        // capture window focus
+        if (onFocus !== null) {
+            $(window).on("focus", onFocus);
+        }
+        if (onUnfocus !== null) {
+            $(window).on("blur", onUnfocus);
+        }
+        // prevent right click
+        $(window).on("contextmenu", (e) => {
+            e.preventDefault();
+            return true;
+        });
+    }
+
     static show(what) {
         $(what).css({
             display: 'block'
